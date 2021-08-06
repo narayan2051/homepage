ed8ca3c2e48b6935800044e3b4ababb58158dcc0
Narayan <narayan@Narayans-MacBook-Pro.local>
2021-08-05 21:36:42 -0500

    Bug fixes

--- W3D4/part2/sum_numbers.js
+++ W3D4/part2/sum_numbers.js
@@ -2,13 +2,13 @@ const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-// readline.question("Enter Number: ",(ans)=>{console.log(ans);       readline.close();
-// });
-sumNumberRecursively = (sum) => {
+
+ const sumNumberRecursively = (sum) => {
   readline.question("Enter Number: ", (num) => {
     if (num === "stop") {
       readline.close();
       console.log("sum=" + sum);
+      return;
     }
     return sumNumberRecursively((sum || 0) + parseInt(num));
   });
